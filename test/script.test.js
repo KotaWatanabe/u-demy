b3f0292f16e4d4da45daa84b48370bab51d1260d
Kota Watanabe <kota3316@gmail.com>
2020-02-25 11:02:24 -0800

    basic google search test

--- test/script.test.js
+++ test/script.test.js
@@ -7,12 +7,25 @@ dbMock = [
     'dogpictures.com'
 ];
 
-it('it is a silly test', () => {
-    expect('hello').toBe('hello')
-    // googleSearch('testtest',dbMock);
+describe('googleSearch',() => {
+    it('it is a silly test', () => {
+        expect('hello').toBe('hello')
+        // googleSearch('testtest',dbMock);
+    })
+    
+    it('it seaching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
+    })
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
 })
 
-it('it seaching google', () => {
-    expect(googleSearch('testtest', dbMock)).toEqual([])
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
-})
+
