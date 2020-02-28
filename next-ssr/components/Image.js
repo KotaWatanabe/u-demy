45b9a9ef41fea486a8e08278494f27aded0a14c4
Kota Watanabe <kota3316@gmail.com>
2020-02-28 14:31:23 -0800

    dynamic app with next.js

---
+++ next-ssr/components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQBPYq3TKOgt6Ffw_fLT4cHlXdufByru9_u_z3aKlC_JLsj3-Xb' />
+)
+
+export default Image;
--- next-ssr/pages/about.js
+++ next-ssr/pages/about.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
-
+import Image from '../components/Image'
 
 const About = () => {
     return (
         <div style={{fontSize:'20px', color:'blue'}}>
             <h1>About</h1>
+            <Image />
             <Link href='/'>
                 <button>Back</button>
             </Link>
