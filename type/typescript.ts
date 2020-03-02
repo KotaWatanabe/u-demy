4b5ab6c79f46e6ad71b98e520037d3ef117a299e
Kota Watanabe <kota3316@gmail.com>
2020-03-01 19:23:42 -0800

    basic types of typescript

--- type/typescript.ts
+++ type/typescript.ts
@@ -1,9 +1,22 @@
-const sum = (a : number, b : number) => {
-     return a + b;
- }
+//boolean
+let isCool: boolean = true
 
- 
+//number
+let age: number = 56;
 
- const answer = sum(4,5);
+//string
+let eyeColor: string = 'brown'
+let favouriteQuote: string = `I'm not old, I'm only ${age}`
 
- console.log(answer);
+// Array
+let pets: string[] = ['cat', 'dog', 'pig']
+let pets2: Array<string> = ['lion', 'dragon', 'lizard']
+
+//Object
+let wizard: object = {
+    a: 'John'
+}
+
+//null and undefined
+let meh: undefined = undefined
+let noo: null = null
