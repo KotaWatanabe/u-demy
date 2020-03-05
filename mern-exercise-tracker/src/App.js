bb0ea99fd31a2001b1a57670cfe95fef285a76c5
Kota Watanabe <kota3316@gmail.com>
2020-03-04 16:59:55 -0800

    setup front end and Navbar

---
+++ mern-exercise-tracker/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/navbar.component'
+
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <br/>
+      {/* <Route path="/" exact component={ExercisesList} /> */}
+      {/* <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} /> */}
+    </Router>
+
+  );
+}
+
+export default App;
