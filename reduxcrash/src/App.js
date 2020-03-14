a062511bd00a01936fc30bb47a3fc0106fb19bf9
Kota Watanabe <kota3316@gmail.com>
2020-03-14 14:13:27 -0700

    fetch posts reducer and action

--- reduxcrash/src/App.js
+++ reduxcrash/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Posts from '../src/components/Posts'
 import PostForm from './components/Postform'
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <PostForm />
-      <hr/>
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr/>
+        <Posts />
+      </div>
+    </Provider>
+
   );
 }
 
