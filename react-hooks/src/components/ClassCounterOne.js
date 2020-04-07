f2dc397723fd1ee11b91fd267c7edaa8d0ea22b4
Kota Watanabe <kota3316@gmail.com>
2020-04-07 14:30:02 -0700

    conditionally run useEffect

--- react-hooks/src/components/ClassCounterOne.js
+++ react-hooks/src/components/ClassCounterOne.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react'
 
 export class ClassCounterOne extends Component {
     state = {
-        count: 0
+        count: 0,
+        name:''
     }
 
     componentDidMount() {
-        console.log('componetDidMount')
         document.title = `Clicked ${this.state.count} times`
     }
-    componentDidUpdate() {
-        console.log('componetWillMount')
-        document.title = `Clicked ${this.state.count} times`
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            console.log('Updating document title')
+            document.title = `Clicked ${this.state.count} times`
+        }
     }
     render() {
-        console.log('render')
         return (
             <div>
                 <button onClick={() => this.setState({count: this.state.count + 1})}>
                     Click {this.state.count} times
                 </button>
+                <input 
+                    type="text" 
+                    value={this.state.name}
+                    onChange={e => this.setState({name: e.target.value})} 
+                />
             </div>
         )
     }
