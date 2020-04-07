6d4b0090fb3fb2f05d6452a7c7df543cde4af4d7
Kota Watanabe <kota3316@gmail.com>
2020-04-07 14:44:19 -0700

    useEffect run only once

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { HookCounterOne } from './components/HookCounterOne';
 import { ClassCounterOne } from './components/ClassCounterOne';
+import { ClasMouse } from './components/ClasMouse';
+import { HookMouse } from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounterOne />
-      <HookCounterOne />
+      {/* <ClassCounterOne />
+      <HookCounterOne /> */}
+      {/* <ClasMouse /> */}
+      <HookMouse />
     </div>
   );
 }
---
+++ react-hooks/src/components/ClasMouse.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class ClasMouse extends Component {
+    state={
+        x:0,
+        y:0
+    }
+
+    logMousePosition = e => {
+        this.setState({x: e.clientX, y: e.clientY })
+    }
+
+    componentDidMount() {
+        window.addEventListener('mousemove', this.logMousePosition)
+    }
+    render() {
+        return (
+            <div>
+                X - {this.state.x} Y - {this. state.y}
+            </div>
+        )
+    }
+}
+
+export default ClasMouse
