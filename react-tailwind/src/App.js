613499f6b578c742f891d44411b9ca5c8b4120cf
Kota Watanabe <kota3316@gmail.com>
2020-04-20 15:13:40 -0700

    imageCard component

--- react-tailwind/src/App.js
+++ react-tailwind/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import ImageCard from './components/ImageCard';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -15,39 +15,12 @@ function App() {
     })
     .catch(err => console.log(err));
   },[])
-
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-x mb-2">
-        Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            3000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            4000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
