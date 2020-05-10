6a1981237994fab4ca754e5bdf35106ee8f9a162
Kota Watanabe <kota3316@gmail.com>
2020-05-10 12:00:39 -0700

    localStorage setItem

--- vanila-js/movie-seat-booking/script.js
+++ vanila-js/movie-seat-booking/script.js
@@ -6,8 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
+
 const updateSelectedCount = () => {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -16,6 +28,7 @@ const updateSelectedCount = () => {
 
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
