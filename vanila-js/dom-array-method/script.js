583127cecd30d76043fab3a60500c11416dd5a5c
Kota Watanabe <kota3316@gmail.com>
2020-05-13 16:55:30 -0700

    DOM array methods

---
+++ vanila-js/dom-array-method/script.js
@@ -0,0 +1,92 @@
+const main = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionairesBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+// fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser)
+}
+
+// Double everyones money
+function doubleMoney() {
+    data = data.map(user => {
+        return {
+            ...user,
+            money: user.money * 2
+        }
+    });
+    updateDOM();
+}
+
+// Sort by wealth
+function sortByWealth() {
+    data.sort((a,b) => b.money - a.money)
+    updateDOM();
+}
+
+// filtering data and showing only millionaires
+function showOnlyMillionaires() {
+    data = data.filter(user => user.money > 1000000)
+    updateDOM();
+}
+
+// Calculate total wealth
+function calculateTotal() {
+    const total = data.reduce((acc, current) => {
+        return acc + current.money
+    },0)
+    const element = document.createElement('div');
+    element.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(total)}</strong> </h3>`;
+    main.appendChild(element)
+}
+
+
+// Add new obj to data arr
+function addData(obj) {
+    data.push(obj);
+    updateDOM();
+}
+
+// Update DOM
+
+function updateDOM(providedData = data) {
+    //Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}<strong> ${formatMoney(item.money)}`;
+        main.appendChild(element)
+    });
+}
+
+// Format number as a money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event listeners
+addUserBtn.addEventListener('click', getRandomUser)
+doubleBtn.addEventListener('click', doubleMoney)
+sortBtn.addEventListener('click', sortByWealth)
+showMillionairesBtn.addEventListener('click', showOnlyMillionaires)
+calculateWealthBtn.addEventListener('click', calculateTotal)
