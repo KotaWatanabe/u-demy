96a41532f5f0cfc9d66ca2ea9a0267e95ebd1f94
Kota Watanabe <kota3316@gmail.com>
2020-05-16 18:16:39 -0700

    full function

--- vanila-js/hangman/script.js
+++ vanila-js/hangman/script.js
@@ -4,15 +4,69 @@ const playAgainBtn = document.getElementById('play-again');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+const hint = document.getElementById('hint')
 
 const figureParts = document.querySelectorAll('.figure-part')
 
-const words = ['application','programming', 'interface', 'wizard'];
+const words = ['nigeria','indonesia', 'wyoming', 'iceland'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const correctLetters = [];
 const wrongLetters = [];
+
+// show hint
+function showHint() {
+    switch(selectedWord) {
+        case 'nigeria':
+        hint.innerText = 'Hint: Biggest population country in Africa'
+    }
+    switch(selectedWord) {
+        case 'indonesia':
+        hint.innerText = 'Hint: 4th biggest population country in the world' 
+    }
+    switch(selectedWord) {
+        case 'wyoming':
+        hint.innerText = 'Hint: Smallest population state in US'
+    }
+    switch(selectedWord) {
+        case 'iceland':
+        hint.innerText = 'Hint: The lowest crime rate in the world'
+    }
+}
+
+// update the wrong letters
+function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = ` 
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>': ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    });
+
+    //Check if lost
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately You lost!';
+        popup.style.display ='flex'
+    }
+}
+
+// Show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
 
 // Show hidden word
 function displayWord() {
@@ -35,4 +89,45 @@ function displayWord() {
     }
 }
 
+//Keydown letter press
+window.addEventListener('keydown', e => {
+    // console.log(e.keyCode)
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+    }
+});
+
+//Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+    popup.style.display = 'none';
+})
+
+showHint();
 displayWord();
