619dd412d12417773a6e275b00695f272b79ae8b
Kota Watanabe <kota3316@gmail.com>
2020-05-27 16:05:01 -0700

    update time function

--- vanila-js/typing-game/script.js
+++ vanila-js/typing-game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingBtn = document.getElementById('setting');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
     return words[Math.floor(Math.random() * (words.length))]
@@ -60,6 +66,29 @@ function updateScore() {
     scoreEl.innerText = score;
 }
 
+//Update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time +'s';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        //End game
+        gameOver();
+    }
+}
+
+// Game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display ='flex';
+}
+
 addWordToDom()
 
 //Event listeners
@@ -70,5 +99,9 @@ text.addEventListener('input', e => {
         e.target.value = "";
         addWordToDom();
         updateScore();
+
+        time += 5;
+
+        updateTime();
     }
 })
