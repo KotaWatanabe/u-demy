9cda96205ee6583ed9d8024e733dc56d424ff203
Kota Watanabe <kota3316@gmail.com>
2020-06-06 21:34:48 -0700

    basic components and services

---
+++ angular/hello-world/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
