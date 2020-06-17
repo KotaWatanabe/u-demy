22ed0f284d5d3bf3323f6c213cdcfbba783d81b4
Kota Watanabe <kota3316@gmail.com>
2020-06-17 15:52:26 -0700

    move ball and break bricks

--- vanila-js/breakout-game/script.js
+++ vanila-js/breakout-game/script.js
@@ -101,6 +101,65 @@ function movePaddle() {
     }
 }
 
+//Move ball on canvas
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // Wall collision (x)
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+    // Wall collision (y)
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    //Paddle collision
+    if(ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed
+    }
+
+    //Bricks collision 
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if(brick.visible) {
+                if(ball.x - ball.size > brick.x && // left brick side check
+                    ball.x + ball.size < brick.x + brick.w && // right brick side check
+                    ball.y + ball.size > brick.y && // top brick side check
+                    ball.y - ball.size < brick.y + brick.h // bottom brick side check
+                ) { 
+                    ball.dy *= -1;
+                    brick.visible = false;
+                    
+                    increaseScore();
+                }
+            }
+        })
+    });
+
+    //Hit bottom wall - Lose
+    if(ball.y + ball.size > canvas.height) {
+        showAllBricks();
+        score = 0;
+    }
+}
+
+//Increase Score
+function increaseScore() {
+    score++;
+
+    if(score % (brickRowCount * brickColumnCount) === 0) {
+        showAllBricks();
+    }
+}
+
+//Make all bricks appear
+function showAllBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => brick.visible = true)
+    })
+}
 
 // Draw everything
 function draw() {
@@ -115,7 +174,7 @@ function draw() {
 //Update canvas drawing animation
 function update() {
     movePaddle();
-
+    moveBall();
     // Draw Everything
     draw();
 
