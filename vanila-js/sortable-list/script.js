e43d5cdecdde089432565cec537a6ce71efd1186
Kota Watanabe <kota3316@gmail.com>
2020-06-18 17:16:14 -0700

    check function

--- vanila-js/sortable-list/script.js
+++ vanila-js/sortable-list/script.js
@@ -1,5 +1,7 @@
 const draggableList = document.getElementById('draggable-list')
 const check = document.getElementById('check')
+const hpCounter = document.getElementById('hp')
+const message = document.getElementById('message')
 
 const populatedCountry = [
     'China',
@@ -13,6 +15,8 @@ const populatedCountry = [
     'Russia',
     'Mexico'
 ];
+
+let hp = 3;
 
 //store Listitems
 const listItems = [];
@@ -44,4 +48,89 @@ function createList() {
 
             draggableList.appendChild(listItem)
         })
+
+        addEventListeners();
 }
+
+function dragStart() {
+    // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');  
+}
+function dragEnter() {
+    // console.log('Event: ', 'dragenter');
+    this.classList.add('over')
+}
+function dragLeave() {
+    // console.log('Event: ', 'dragleave');
+    this.classList.remove('over')
+}
+function dragOver(e) {
+    // console.log('Event: ', 'dragover');
+    e.preventDefault();
+
+}
+function dragDrop() {
+    // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over')
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable')
+    const itemTwo = listItems[toIndex].querySelector('.draggable')  
+    
+    listItems[fromIndex].appendChild(itemTwo)
+    listItems[toIndex].appendChild(itemOne);    
+}
+
+function checkOrder() {
+    listItems.forEach((item,index) => {
+        const countryName = item.querySelector('.draggable').innerText.trim();
+
+        if(countryName !== populatedCountry[index]) {
+            item.classList.add('wrong')
+        } else {
+            item.classList.remove('wrong')
+            item.classList.add('right')
+        }
+    })
+    checkWinorLose()
+}
+
+function checkWinorLose() {
+    const checkArr = [];
+    listItems.forEach(item => {
+        if(item.classList.contains('wrong')){
+            checkArr.push(item)
+        }
+    })
+    if(checkArr.length > 0) {
+        hp--;
+    }
+    if(hp <= 0) {
+        message.innerText = "Game over!!!"
+    }
+    hpCounter.innerText = hp
+}
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    })
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    })
+}
+
+check.addEventListener('click',checkOrder)
+
+//hp
+hpCounter.innerText = hp
