ce1127231a9a0adbf31cdf99910a7fb7ec0c535f
Kota Watanabe <kota3316@gmail.com>
2020-07-30 15:11:43 -0700

    firebase config and uploadForm component

---
+++ firegram-starter-files/src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBDHE_S5XSyen1AYcFY_msaHYLjVK6SXWY",
+  authDomain: "firegram-601b7.firebaseapp.com",
+  databaseURL: "https://firegram-601b7.firebaseio.com",
+  projectId: "firegram-601b7",
+  storageBucket: "firegram-601b7.appspot.com",
+  messagingSenderId: "572327191100",
+  appId: "1:572327191100:web:024cb4294539f194795939"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export {
+  projectStorage,
+  projectFirestore;
+}
