ca1dd49ace3d35e1b04516eb82c5b56dcf20c1ac
Kota Watanabe <kota3316@gmail.com>
2020-08-24 15:05:49 -0700

    video detail component

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -7,16 +7,29 @@ import { SearchBar, VideoLists, VideoDetail } from './components';
 import youtube from './api/youtube';
 
 export default class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  onFormSubmit = async searchTerm => {
+    const response = await youtube.get('search', { params: { q: searchTerm } });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
   render() {
+    const { selectedVideo } = this.state;
     return (
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.onFormSubmit} />
             </Grid>
             <Grid item xs={8}>
-              <VideoDetail />
+              <VideoDetail video={selectedVideo} />
             </Grid>
             <Grid item xs={4}>
               <VideoLists />
