6b5e09ea1cb300b0ed1a06ee08b01dc469580b31
Kota Watanabe <kota3316@gmail.com>
2020-09-02 14:31:16 -0700

    quizbox component and basic functions

---
+++ quiz-vue/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Header/>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox v-if="questions.length" :next="next" :currentQuestion="questions[index]"/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index += 1;
+    }
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple")
+      .then(res => res.json())
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
