ecea31ea6c16e924bae11abf1582afd1c35f9de5
Kota Watanabe <kota3316@gmail.com>
2020-09-02 15:52:15 -0700

    full quiz qpp features

--- quiz-vue/src/components/QuestionBox.vue
+++ quiz-vue/src/components/QuestionBox.vue
@@ -10,21 +10,36 @@
           v-for="(answer, index) in answers"
           :key="index"
           @click="selectAnswer(index)"
+          :class="answerClass(index)"
         >{{ answer }}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 export default {
   // props: ["currentQuestion"]
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    };
   },
   computed: {
     answers() {
@@ -33,9 +48,53 @@ export default {
       return answers;
     }
   },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+        this.answered = false;
+      }
+    }
+  },
   methods: {
     selectAnswer(index) {
-      console.log(index);
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    submitAnswer() {
+      let isCorrect = false;
+
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+      this.answered = true;
+      this.increment(isCorrect);
+    },
+    answerClass(index) {
+      let answerClass = "";
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = "selected";
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = "correct";
+      } else if (
+        this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = "incorrect";
+      }
+      return answerClass;
     }
   }
 };
@@ -47,10 +106,19 @@ export default {
   cursor: pointer;
 }
 .list-group-item:hover {
-  background-color: rgba(0, 0, 0, 0.25);
+  background-color: rgba(0, 0, 0, 0.1);
 }
 .btn {
   margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
 }
 </style>
 
