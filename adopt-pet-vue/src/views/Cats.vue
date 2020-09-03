e37882974e73929936c67800c9d9d4650d47ddee
Kota Watanabe <kota3316@gmail.com>
2020-09-03 16:29:58 -0700

    state management with vuex

---
+++ adopt-pet-vue/src/views/Cats.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats">
+      <!-- <template slot="name" slot-scope="data">
+        <router-link :to="`#/pets/${data.value}`">{{ data.value }}</router-link>
+      </template>-->
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/cats/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["cats"])
+  }
+};
+</script>
