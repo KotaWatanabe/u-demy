6828c7420a20e664929925e63baf00eefa88e7dd
Kota Watanabe <kota3316@gmail.com>
2020-09-08 16:41:49 -0700

    fetchTodos, addTodos, deleteTodo functions

---
+++ vuex-todo/src/store/modules/todos.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+// import axios from 'axios';
+
+const state = {
+  todos: []
+};
+
+const getters = {
+  allTodos: state => state.todos
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+
+    commit('setTodos', response.data);
+  },
+
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      'https://jsonplaceholder.typicode.com/todos',
+      {
+        title,
+        completed: false
+      }
+    );
+
+    commit('newTodo', response.data);
+  },
+
+  async deleteTodo({ commit }, id) {
+    const response = await axios.delete(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+
+    commit('removeTodo', id, response.data);
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter(todo => todo.id !== id))
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
